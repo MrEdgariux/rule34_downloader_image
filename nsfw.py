66262a21478a01a282ea6e738d539e72fdc05194
MrEdgariux <38187996+MrEdgariux@users.noreply.github.com>
2023-03-06 15:41:41 +0200

    Freeze fixes!
     * From now you can use our software without freezes as I see.
    
     It is not tested yet, so any patches might be needed to releases after a test.

--- nsfw.py
+++ nsfw.py
@@ -4,6 +4,7 @@ import requests, os
 import customtkinter as ctk
 import tkinter.messagebox as msg
 import threading
+import sys
 import xml.etree.ElementTree as ET
 
 ctk.set_appearance_mode("System")
@@ -27,6 +28,19 @@ def download_image(image_url, location, image_id, sema):
         sema.release()
     finally:
         sema.release()
+
+def killdw():
+    """Kills all downloads"""
+    confirmation = msg.askyesno("Kill Threads", "Are you sure that you want to kill all downloading content?")
+    if confirmation:
+        confirmation2 = msg.askyesnocancel("Are you really sure?", "IF YOU PROCEED, IT MIGHT LEAD TO CORRUPTED IMAGES! ARE YOU REALLY SURE?")
+        if confirmation2:
+            print("Killing application")
+            sys.exit()
+        else:
+            return
+    else:
+        return
         
 
 def download(ats, search_term):
@@ -66,32 +80,53 @@ def download(ats, search_term):
     
     msg.showinfo("Downloading", "Successfully started to download " + str(skaiciavimas) + " images!")
 
+def my_thread(tags):
+    atsakas = []
+    p = 0
+    atsa = 0
+    while True:
+        ats = requests.get('https://api.rule34.xxx/index.php?page=dapi&tags=' + tags + '&s=post&q=index&json=1&limit=1000&pid=' + str(p))
+        if ats == "":
+            errors.configure(text="No content was found!", font= ('Helvetica 13'))
+            break
+        else:
+            try:
+                atsj = ats.json()
+                atsakas.extend(atsj)
+                if (len(atsj) < 1000):
+                    atsa += len(atsj)
+                    info.configure(text="We found " + str(atsa) + " content to download, waiting for confirmation", font= ('Helvetica 13'))
+                    # Ask for confirmation
+                    confirmation = msg.askyesno("Start download?", "We found " + str(atsa) + " content, do you want download it?")
+
+                    # Check the user's response
+                    if confirmation:
+                        print("You have confirmed, that you want to download " + str(atsa) + " content of " + tags)
+                        download(atsakas, tags)
+                        info.configure(text="", font= ('Helvetica 13'))
+                        break
+                    else:
+                        print("You have cancelled download " + str(atsa) + " content of " + tags)
+                        errors.configure(text="You have cancelled your operation", font= ('Helvetica 13'))
+                        info.configure(text="", font= ('Helvetica 13'))
+                        break
+                info.configure(text="We found " + str(atsa) + " content to download, loading more...", font= ('Helvetica 13'))
+                p += 1
+                atsa += 1000
+                print(str(p) + " sekmingai pratęstas! Atsiųsta iš viso " + str(atsa))
+            except JSONDecodeError:
+                errors.configure(text="No content was found!", font= ('Helvetica 13'))
+                break
+
 def data():
     atsakas = []
     tags = text_1.get()
     p = 0
-    atsa = 0
     print(tags)
     if (checkbox_1.get() == 1):
-        while True:
-            ats = requests.get('https://api.rule34.xxx/index.php?page=dapi&tags=' + tags + '&s=post&q=index&json=1&limit=1000&pid=' + str(p))
-            if ats == "":
-                errors.configure(text="No content was found!", font= ('Helvetica 13'))
-                break
-            else:
-                try:
-                    atsj = ats.json()
-                    atsakas.extend(atsj)
-                    if (len(atsj) < 1000):
-                        atsa += len(atsj)
-                        download(atsakas, tags)
-                        break
-                    p += 1
-                    atsa += 1000
-                    print(str(p) + " sekmingai pratęstas! Atsiųsta iš viso " + str(atsa))
-                except JSONDecodeError:
-                    errors.configure(text="No content was found!", font= ('Helvetica 13'))
-                    break
+        t = threading.Thread(target=my_thread, args=(tags,))
+        t.start()
+        info.configure(text="Content will be searched and this message will be updated with information!", font= ('Helvetica 13'))
     else:
         pagea = page.get()
         if not pagea:
@@ -117,6 +152,11 @@ def data():
                     return
             download(atsakas, tags)
 
+def toggle_entry_state():
+    if checkbox_1.get() == True:
+        page.pack_forget()
+    else:
+        page.pack(pady=20, padx=10)
 
 
 ct = ctk.CTk()
@@ -126,22 +166,30 @@ ct.title("Rule 34 downloader")
 ct.geometry(f"{1100}x{580}")
 
 frame_1 = ctk.CTkFrame(master=ct)
-frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+frame_2 = ctk.CTkFrame(master=ct)
+frame_1.pack(pady=20, padx=30, fill="both", expand=True)
+frame_2.pack(pady=20, padx=30, fill="both", expand=True)
 
 errors = ctk.CTkLabel(master=frame_1, text="", text_color="red")
 errors.pack(pady=10, padx=10)
 
-text_1 = ctk.CTkEntry(master=frame_1, placeholder_text="What tags do you preffer?", width=300)
+info = ctk.CTkLabel(master=frame_1, text="")
+info.pack(pady=10, padx=10)
+
+text_1 = ctk.CTkEntry(master=frame_2, placeholder_text="What tags do you preffer?", width=300)
 text_1.pack(pady=10, padx=10)
 
-page = ctk.CTkEntry(master=frame_1, placeholder_text="Pages (leave blank if download everything)", width=300)
+page = ctk.CTkEntry(master=frame_2, placeholder_text="Page to download", width=300)
 page.pack(pady=20, padx=10)
 
-checkbox_1 = ctk.CTkCheckBox(master=frame_1, text="Download everything (it might crash software)")
+checkbox_1 = ctk.CTkCheckBox(master=frame_2, text="Download everything", command=toggle_entry_state)
 checkbox_1.pack(pady=10, padx=10)
 
-button_1 = ctk.CTkButton(master=frame_1, command=data)
+button_1 = ctk.CTkButton(master=frame_2, text="Start", command=data)
 button_1.pack(pady=10, padx=10)
+
+button_em = ctk.CTkButton(master=frame_2, text="Kill download", command=killdw)
+button_em.pack(pady=10, padx=10)
 try:
     ct.mainloop()
 except KeyboardInterrupt:
